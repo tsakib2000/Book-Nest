61ee0eff59aff2f19fe7279f6a2a318f1be225c3
tsakib2000 <isakib49@gmail.com>
2025-06-04 23:19:48 +0600

    first commit

---
+++ Client/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDl572lUt4PeFxpUC6a1PJBsU4ZlTL3hsU",
+  authDomain: "book-nest-9a2d1.firebaseapp.com",
+  projectId: "book-nest-9a2d1",
+  storageBucket: "book-nest-9a2d1.firebasestorage.app",
+  messagingSenderId: "821401621070",
+  appId: "1:821401621070:web:2544175c79b8ac191c291f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
